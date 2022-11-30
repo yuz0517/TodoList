39c007fa695561fe243a91134f99432530127dc5
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2022-11-30 23:55:26 +0900

    fix Item.js

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BiCircle, BiCheckCircle, BiMinusCircle} from 'react-icons/bi'
 import './Item.scss'
+import cn from 'classnames'
+import { IoMdCheckboxOutline } from 'react-icons/io';
 const Item = (todo) => {
     const { text, checked } = todo;
     return(
         <div className='div-Item'>
-            <div className='div-checkbox'>
-                <BiCircle/>
+            <div className={cn('div-checkbox',{ checked })}>
+                {checked ? <BiCheckCircle /> : <BiCircle />}
+                
                 <div className='div-text'>할 일</div>
             </div>
             <div className='remove'>
