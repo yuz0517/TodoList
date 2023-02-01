eb2d84b671679e315808228f475634374198d858
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-02-01 23:18:08 +0900

    add firestore update, delete

--- src/Components/TodoPage.js
+++ src/Components/TodoPage.js
@@ -2,11 +2,19 @@ import React, { useEffect, useState, useRef, useCallback } from 'react';
 import Template from './Template';
 import InputList from './InputList';
 import Logout from './Login/Logout';
+import Calendar from 'react-calendar';
 import List from './List';
-import { db } from './../firebase.js'
-import { collection, getDocs, getDoc, Timestamp } from 'firebase/firestore'
+import { db } from './../firebase.js';
+import {
+  collection,
+  getDocs,
+  getDoc,
+  Timestamp,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
-
 
 const TodoPage = () => {
   const [tasks, setTasks] = useState([
@@ -17,100 +25,127 @@ const TodoPage = () => {
     },
   ]);
 
-
   const [todos, setTodos] = useState([
     {
       taskid: '',
-      date: {nanoseconds: '', seconds:''},
+      date: { nanoseconds: '', seconds: '' },
       task: '',
       useID: '',
       isCompleted: false,
-    }
+    },
   ]);
 
-  const todoCollectionRef = collection(db,"todos");
-  var todaysDate= (new Date()).toString().slice(4,15);
+  const todoCollectionRef = collection(db, 'todos');
+  var todaysDate = new Date().toString().slice(4, 15);
 
-  
   var length = 0;
   const getTodos = async () => {
     const data = await getDocs(todoCollectionRef);
-    //console.log(data.docs.map((doc) => ({...doc.data(), id:doc.id}))  );
-    const defaultdatalength = (data.docs.map((doc) => ({...doc.data(), id:doc.id}))).length; //firestore로 불러온 데이터의 길이
+
+    const defaultdatalength = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    })).length; //firestore로 불러온 데이터의 길이
     length = defaultdatalength;
-    const datetime = new Date().getDate() ;
-    const newtodos=((data.docs.map((doc) => ({...doc.data(), id:doc.id}))).map((v,index) => ({...v, taskid:index}))
-    .filter(todo => (todo.useID) == sessionStorage.key(0)))//현재 로그인한 사용자별로 필터링.
+    const datetime = new Date().getDate();
+    const firestoretodos = data.docs
+      .map((doc) => ({ ...doc.data(), id: doc.id }))
+      .map((v, index) => ({ ...v, taskid: index }))
+      .filter((todo) => todo.useID == sessionStorage.key(0)); //현재 로그인한 사용자별로 필터링.
 
-    console.log(Date(newtodos[1].date.seconds).slice(4,15))
+    console.log(Date(firestoretodos[1].date.seconds).slice(4, 15));
 
-    var todayDate= (new Date()).toString().slice(4,15);
+    //var todayDate= (new Date()).toString().slice(4,15);
     //const newnewtodos = newtodos.filter(todo => (todo.date.seconds) >=  (new Date("01/31/2023").getTime()/1000)  )
-    const newnewtodos = newtodos.filter(todo => (todo.date.seconds) >=  (((new Date().getMonth() + 1) + "/" + (new Date().getDate()) +"/"+(new Date().getFullYear())).getTime()/1000)  )
+    let todaydate =
+      new Date().getMonth() +
+      1 +
+      '/' +
+      new Date().getDate() +
+      '/' +
+      new Date().getFullYear();
+    todaydate = new Date(todaydate).getTime() / 1000;
+    const newtodos = firestoretodos.filter(
+      (todo) => todo.date.seconds >= todaydate,
+    );
+    setTodos(newtodos);
     //console.log(newDate)
     //const newtodos = todos.filter((todo) => Date(todo.date.seconds).toString().includes("Jan 31"))
-   // console.log("todos",newtodos);
+    // console.log("todos",newtodos);
     // setTodos((data.docs.map((doc) => ({...doc.data(), id:doc.id}))).map((v,index) => ({...v, taskid:index})).filter(
     //    todo => (todo.date).toString().slice(4,11) !==   (todaysDate)));
     //console.log(Date(todos[5].date.seconds).toString().slice(4,15))
     //console.log(todos.filter(todo => Date(todo.date.seconds).toString().slice(4,15) === (todaysDate)))
-    
-    //setTodos(todos.filter((todo) => Date(todo.date.seconds).toString().includes(todaysDate)))
-    
 
-    console.log(todos)
-  }
+    //setTodos(todos.filter((todo) => Date(todo.date.seconds).toString().includes(todaysDate)))
+
+    console.log(todos);
+  };
 
   function getMovies() {
     const getDB = collection(db, 'todos');
     getDocs(getDB)
-      .then(response => {
-        console.log(response)
-
-    })
-    .catch(error => console.log(error.message));
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error.message));
   }
-  
+
   const navigate = useNavigate();
 
   useEffect(() => {
     getTodos(); // firestore로부터 온 데이터
     getMovies();
-    
-     // setTasks(
+
+    // setTasks(
     //   tasks.map((task) =>
     //     task.id === id ? { ...task, checked: !task.checked } : task,
     //   ),
-   
+
     if (sessionStorage.length === 0) {
       console.log(sessionStorage.length);
       navigate('/');
-  }
+    }
     return () => {};
   }, []);
-  //const newtodos = todos.filter((todo) => Date(todo.date.seconds).toString().includes("Jan 29"))
-  //const newtodos = todos.filter((todo) => Date(todo.task).includes("1월"))
 
+  console.log(todos);
 
-  //console.log("includes",todos.filter((todo) => (todo.task).includes("1월"))) //정상적으로 표시
-  //console.log("includes",todos.filter((todo) => (Date(todo.date.seconds).slice(4,15)).toLowerCase().includes('Jan 31'))) //정상적으로 표시
-  //console.log(todaysDate.includes("Jan"))
-  console.log(todos)
-  
+  const nextId = useRef(todos.length); //바꿔주기
 
-  const nextId = useRef(11);//바꿔주기
+  const updateTodos = async (updatetask) => {
+    const todoCollectionRef = doc(db, 'todos', updatetask.id);
+    if (updatetask.isCompleted === true) {
+      await updateDoc(todoCollectionRef, {
+        isCompleted: false,
+      });
+    }else if (updatetask.isCompleted === false) {
+      await updateDoc(todoCollectionRef, {
+        isCompleted: true,
+      })
+    }
+  };
   const onDone = useCallback(
     (taskid) => {
+      const updatetask = todos.find((todo) => todo.taskid === taskid);
+      updateTodos(updatetask);
       setTodos(
         todos.map((todo) =>
-          todo.taskid === taskid ? { ...todo, isCompleted: !todo.isCompleted } : todo,
+          todo.taskid === taskid
+            ? { ...todo, isCompleted: !todo.isCompleted }
+            : todo,
         ),
       );
     },
     [todos],
   );
+  const deleteTodos = async (deletetask) => {
+    await deleteDoc(doc(db, 'todos', deletetask.id));
+  };
   const onRemove = useCallback(
     (taskid) => {
+      const deletetask = todos.find((todo) => todo.taskid === taskid);
+      deleteTodos(deletetask);
       setTodos(todos.filter((todo) => todo.taskid !== taskid));
     },
     [todos],
@@ -131,6 +166,7 @@ const TodoPage = () => {
 
   return (
     <div>
+      <Calendar />
       <Logout />
       <Template>
         <InputList onInsert={onInsert}></InputList>
