71ba071a492e42246b67fa964fb3b1dc5f7825a9
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-02-02 11:01:45 +0900

    add calendar

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,15 +1,18 @@
 
 
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+//import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.scss';
+import './Calendar.scss'
 function Calendar() {
   const [value, onChange] = useState(new Date());
 
   return (
     <div>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar onChange={onChange} value={value} className={'react-calendar'}    />
     </div>
   );
 }
 export default Calendar;
+
+
